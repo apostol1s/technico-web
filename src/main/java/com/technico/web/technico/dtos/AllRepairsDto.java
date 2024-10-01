efda7b455a89368b44db12f698077ad0d5179d36
Apostolos Tourlidas <tourlidasapostolis@gmail.com>
2024-10-01 23:17:44 +0300

    @JsonFormat at AllRepairsDto

--- src/main/java/com/technico/web/technico/dtos/AllRepairsDto.java
+++ src/main/java/com/technico/web/technico/dtos/AllRepairsDto.java
@@ -1,5 +1,6 @@
 package com.technico.web.technico.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.technico.web.technico.models.RepairStatus;
 import com.technico.web.technico.models.RepairType;
 import java.math.BigDecimal;
@@ -18,15 +19,20 @@ public class AllRepairsDto {
     private String e9;
     private RepairType repairType;
     private String shortDescription;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date submissionDate;
     private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date scheduledStartDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date scheduledEndDate;
     private BigDecimal proposedCost;
     private Boolean acceptanceStatus;
     private RepairStatus repairStatus;
     private String repairAddress;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date actualStartDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private Date actualEndDate;
     private Boolean deleted;
 }
